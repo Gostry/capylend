b8a2bf95a98a631ea6e176eda1ca46fb412fb2ff
Gostry <cobtahacademy21@gmail.com>
2025-12-25 16:59:55 +0200

    ваіпп

--- input.js
+++ input.js
@@ -1,4 +1,35 @@
-let input = document.getElementById("input");
-function button1() {
-    document.body.style.backgroundColor = input.value;
+let red = document.getElementById("red");
+let green = document.getElementById("green");
+let blue = document.getElementById("blue");
+let a = document.getElementById("a");
+let h1 = document.getElementById("h1");
+
+red.addEventListener("input", red1);
+green.addEventListener("input", green1);
+blue.addEventListener("input", blue1);
+a.addEventListener("input", a1);
+
+function red1() {
+    document.body.style.backgroundColor =
+        "rgba(" + red.value + "," + green.value + "," + blue.value + "," + a.value + ")";
+    document.getElementById("h1").textContent =
+        "rgba(" + red.value + "," + green.value + "," + blue.value + "," + a.value + ")";
+}
+function green1() {
+    document.body.style.backgroundColor =
+        "rgba(" + red.value + "," + green.value + "," + blue.value + "," + a.value + ")";
+    document.getElementById("h1").textContent =
+        "rgba(" + red.value + "," + green.value + "," + blue.value + "," + a.value + ")";
+}
+function blue1() {
+    document.body.style.backgroundColor =
+        "rgba(" + red.value + "," + green.value + "," + blue.value + "," + a.value + ")";
+    document.getElementById("h1").textContent =
+        "rgba(" + red.value + "," + green.value + "," + blue.value + "," + a.value + ")";
+}
+function a1() {
+    document.body.style.backgroundColor =
+        "rgba(" + red.value + "," + green.value + "," + blue.value + "," + a.value + ")";
+    document.getElementById("h1").textContent =
+        "rgba(" + red.value + "," + green.value + "," + blue.value + "," + a.value + ")";
 }
